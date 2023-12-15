0c87ae2f5f3402c57b84f58149b9e28f273da0cd
H J <qkrwlgml33@gmail.com>
2023-12-15 18:38:51 +0900

    user app 전체적인 코드 수정

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import UserView
+from .views import UserDetailView
 
 from . import views
 
 # TODO: 내정보 수정, 회원탈퇴, 블랙리스트, VIP리스트
 urlpatterns = [
-    path("<int:user_id>", views.index, name="index"),
-    path("<str:user_name>", views.UserView.as_view())
+    path("<int:user_id>", UserDetailView.as_view())
 ]
